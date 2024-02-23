3e05f81901387eb2a87bed72980b9fc7a1476449
Ceyhun <47317865+Ezelceyhun@users.noreply.github.com>
2024-02-23 12:32:39 +0300

    proje1

---
+++ Login.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Kütüphane_Otomasyonu
+{
+    partial class Login
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.admin_k = new Guna.UI2.WinForms.Guna2TextBox();
+            this.admin_s = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.bildiri_label = new System.Windows.Forms.Label();
+            this.isim_label = new System.Windows.Forms.Label();
+            this.sifre_label = new System.Windows.Forms.Label();
+            this.guna2CircleButton1 = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.SuspendLayout();
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.BorderRadius = 50;
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // admin_k
+            // 
+            this.admin_k.BorderRadius = 5;
+            this.admin_k.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.admin_k.DefaultText = "";
+            this.admin_k.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.admin_k.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.admin_k.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.admin_k.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.admin_k.FillColor = System.Drawing.Color.WhiteSmoke;
+            this.admin_k.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.admin_k.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.admin_k.ForeColor = System.Drawing.Color.DimGray;
+            this.admin_k.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.admin_k.Location = new System.Drawing.Point(111, 99);
+            this.admin_k.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.admin_k.Name = "admin_k";
+            this.admin_k.PasswordChar = '\0';
+            this.admin_k.PlaceholderText = "";
+            this.admin_k.SelectedText = "";
+            this.admin_k.Size = new System.Drawing.Size(173, 46);
+            this.admin_k.TabIndex = 0;
+            // 
+            // admin_s
+            // 
+            this.admin_s.BorderRadius = 5;
+            this.admin_s.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.admin_s.DefaultText = "";
+            this.admin_s.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.admin_s.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.admin_s.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.admin_s.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.admin_s.FillColor = System.Drawing.Color.WhiteSmoke;
+            this.admin_s.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.admin_s.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.admin_s.ForeColor = System.Drawing.Color.DimGray;
+            this.admin_s.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.admin_s.Location = new System.Drawing.Point(111, 177);
+            this.admin_s.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.admin_s.Name = "admin_s";
+            this.admin_s.PasswordChar = '\0';
+            this.admin_s.PlaceholderText = "";
+            this.admin_s.SelectedText = "";
+            this.admin_s.Size = new System.Drawing.Size(173, 46);
+            this.admin_s.TabIndex = 1;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BorderRadius = 10;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.Location = new System.Drawing.Point(111, 230);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(173, 45);
+            this.guna2Button1.TabIndex = 2;
+            this.guna2Button1.Text = "Login";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // bildiri_label
+            // 
+            this.bildiri_label.AutoSize = true;
+            this.bildiri_label.Location = new System.Drawing.Point(157, 305);
+            this.bildiri_label.Name = "bildiri_label";
+            this.bildiri_label.Size = new System.Drawing.Size(72, 16);
+            this.bildiri_label.TabIndex = 3;
+            this.bildiri_label.Text = "bildiri label";
+            // 
+            // isim_label
+            // 
+            this.isim_label.AutoSize = true;
+            this.isim_label.Font = new System.Drawing.Font("AvenirNext LT Pro Regular", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.isim_label.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.isim_label.Location = new System.Drawing.Point(120, 71);
+            this.isim_label.Name = "isim_label";
+            this.isim_label.Size = new System.Drawing.Size(155, 24);
+            this.isim_label.TabIndex = 4;
+            this.isim_label.Text = "Kullanıcı İsminiz";
+            // 
+            // sifre_label
+            // 
+            this.sifre_label.AutoSize = true;
+            this.sifre_label.Font = new System.Drawing.Font("AvenirNext LT Pro Regular", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sifre_label.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.sifre_label.Location = new System.Drawing.Point(156, 149);
+            this.sifre_label.Name = "sifre_label";
+            this.sifre_label.Size = new System.Drawing.Size(76, 24);
+            this.sifre_label.TabIndex = 5;
+            this.sifre_label.Text = "Şifreniz";
+            // 
+            // guna2CircleButton1
+            // 
+            this.guna2CircleButton1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButton1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButton1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2CircleButton1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2CircleButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2CircleButton1.ForeColor = System.Drawing.Color.White;
+            this.guna2CircleButton1.Location = new System.Drawing.Point(346, 12);
+            this.guna2CircleButton1.Name = "guna2CircleButton1";
+            this.guna2CircleButton1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CircleButton1.Size = new System.Drawing.Size(47, 47);
+            this.guna2CircleButton1.TabIndex = 6;
+            this.guna2CircleButton1.Click += new System.EventHandler(this.guna2CircleButton1_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(404, 404);
+            this.Controls.Add(this.guna2CircleButton1);
+            this.Controls.Add(this.sifre_label);
+            this.Controls.Add(this.isim_label);
+            this.Controls.Add(this.bildiri_label);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.admin_s);
+            this.Controls.Add(this.admin_k);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Guna.UI2.WinForms.Guna2TextBox admin_k;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2TextBox admin_s;
+        private System.Windows.Forms.Label bildiri_label;
+        private System.Windows.Forms.Label isim_label;
+        private System.Windows.Forms.Label sifre_label;
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton1;
+    }
+}
+
---
+++ Login.cs
@@ -0,0 +1,57 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kütüphane_Otomasyonu
+{
+    public partial class Login : Form
+    {
+        MySqlConnection con = new MySqlConnection("Server=auth-db1017.hstgr.io;Database=u918363033_kutuphane;user=u918363033_kutuphane;Pwd=Cumhuriyet21;");
+        MySqlCommand cmd;
+        MySqlDataReader dr;
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            string sorgu = "SELECT * FROM admin_kullanicilar where admin_k=@k AND admin_s=@sifre";
+            cmd = new MySqlCommand(sorgu, con);
+            cmd.Parameters.AddWithValue("@k", admin_k.Text);
+            cmd.Parameters.AddWithValue("@sifre", admin_s.Text);
+            con.Open();
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                admin_panel admin_Panel = new admin_panel();
+                admin_Panel.Show();
+                this.Hide();
+            }
+            /*
+            else
+            {
+                bildiri_label.Text = "Bilgileri Kontrol Edin !";
+            }
+            */
+        }
+
+        private void guna2CircleButton1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
